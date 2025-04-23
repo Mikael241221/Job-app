772b1a34038f2c72b2a748172c9c8ef8e86607b1
Mikael241221 <bashawdejenu1221@gmail.com>
2025-04-23 05:38:30 -0700

    styling the JobCard pages

--- src/components/Joblisting.jsx
+++ src/components/Joblisting.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
-import { assets, JobCategories, JobLocations, jobsData } from '../assets/assets'
+import { assets, JobCategories, JobLocations} from '../assets/assets'
 import JobCard from './JobCard'
 
 const Joblisting = () => {
-    const {isSearched, searchFilter,setSearchFilter} = useContext(AppContext)
+    const {isSearched, searchFilter,setSearchFilter,jobs} = useContext(AppContext)
    
   return (
     <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
@@ -72,7 +72,7 @@ const Joblisting = () => {
         <p className='mb-8'>Get your desired job from top companies</p>
         <div className='grid grid-cols-1 sm:grid-cols-2 xl-grid-cols-3 gap-4'>
          {
-          jobsData.map((job,index) => (
+          jobs.map((job,index) => (
             <JobCard key={index} job={job}/>
           ))
          }
