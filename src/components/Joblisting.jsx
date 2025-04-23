60ee43e5ce4857e7cdfbf2a07f7591a8eb385ff9
Mikael241221 <bashawdejenu1221@gmail.com>
2025-04-23 04:44:26 -0700

    create new component

--- src/components/Joblisting.jsx
+++ src/components/Joblisting.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
-import { assets, JobCategories, JobLocations } from '../assets/assets'
+import { assets, JobCategories, JobLocations, jobsData } from '../assets/assets'
+import JobCard from './JobCard'
 
 const Joblisting = () => {
     const {isSearched, searchFilter,setSearchFilter} = useContext(AppContext)
@@ -51,7 +52,7 @@ const Joblisting = () => {
         {/* Location filter */}
         <div className='max-lg:hidden'>
           
-            <h4 className='font-medium text-lg py-4'>Search by Location</h4>
+            <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
             <ul className='space-y-4 text-gray-600'>
               {
                 JobLocations.map((location,index) =>(
@@ -69,8 +70,12 @@ const Joblisting = () => {
       <section className='w-full lg:w-3/4 to-gray-800 max-lg:px-4'>
         <h3 className='font-medium text-3xl py-2' id='job-list'>Latest jobs</h3>
         <p className='mb-8'>Get your desired job from top companies</p>
-        <div className='grid grid-cols-1 sm:'>
-
+        <div className='grid grid-cols-1 sm:grid-cols-2 xl-grid-cols-3 gap-4'>
+         {
+          jobsData.map((job,index) => (
+            <JobCard key={index} job={job}/>
+          ))
+         }
         </div>
       </section>
     </div>
